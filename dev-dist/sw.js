cc119f1c945d47bb154256c937cdb8d66cf17c3b
ceslep <ceslep@users.noreply.github.com>
2026-06-22 20:28:57 -0500

    ght prueba 23 de junio

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-650259a1'], (function (workbox) { 'use strict';
     "revision": "2e9e9e0ed77a63a0797c19e34461a2d9"
   }, {
     "url": "/polla/index.html",
-    "revision": "0.u6rbrrmaoj4"
+    "revision": "0.2gn0u1adaeo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/polla/index.html"), {
